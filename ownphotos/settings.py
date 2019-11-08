c9046053d9670edbba65ea722816cacf5019e51a
Jacob Gallagher <jacobpgallagher@gmail.com>
2019-11-07 20:26:51 -0700

    Move static so it doesn't conflict with frontend

--- ownphotos/settings.py
+++ ownphotos/settings.py
@@ -268,7 +268,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATIC_URL = '/django_static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'protected_media')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
